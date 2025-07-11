da63d01ac8a185ed1dd272c1ef4947bd3591ed50
calledhxx <calledhxx@gmail.com>
2025-07-11 19:18:58 +0800

    hxx.lol

--- index.js
+++ index.js
@@ -1,8 +1,204 @@
 let CubeSideSize = 220;
 let HTTPService = new XMLHttpRequest();
 
+let isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', function () {
+    isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    PaintUp();
+});
+
+const Colors = {
+    "Dark":{
+        "CLICKON_DynamicBubblesBackground" : "rgba(53,53,53,0.9)",
+        "CLICKON_DynamicBubblesBorder" :"rgba(46,46,46,0.9)",
+        "CLICKON_DynamicBubblesShadow" :"0 0 40px 5px rgba(60, 60, 60, 1)",
+
+        "DynamicBubblesBackground" : "rgba(67,67,67,0.7)",
+        "DynamicBubblesBorder" :"rgba(53,53,53,0.7)",
+        "DynamicBubblesShadow" :"0 0 40px 5px rgba(72, 72, 72, 1)",
+
+        "BlackChunk":"#1c1c1c",
+        "WhiteChunk":"#787878",
+
+        "DBTitle": "#cdcdcd",
+        "DBContent" : "#c5c5c5",
+        "DBType" : "#c8c8c8",
+        "DBBottomBar" : "#cdcdcd",
+
+        "DBTextShadow" : "rgba(64, 64, 64, 0.75) 0 0 20px",
+
+        "Background" : "#272727",
+
+        "DMControlBarBackground":"rgba(80,80,80,0.5)",
+        "DMControlBarShadow":"0 0 10px 10px rgba(78,78,78, 0.2)",
+        "DMControlBarBorder":"rgba(85,85,85,0.75)",
+
+        "DMControlBarMidBackground" : "rgb(108,108,108)",
+        "DMControlBarMidShadow" : "0 0 10px 10px rgba(53,53,53, 0.73)",
+
+        "DMControlBarRopeBackground" : "rgb(108,108,108)",
+        "DMControlBarGripBorder" : "rgb(108,108,108)",
+    },
+    "Light":{
+        "CLICKON_DynamicBubblesBackground" : "rgba(236,236,236,0.9)",
+        "CLICKON_DynamicBubblesBorder" :"rgba(218,218,218,0.9)",
+        "CLICKON_DynamicBubblesShadow" :"0 0 40px 5px rgba(184, 184, 184, 1)",
+
+        "DynamicBubblesBackground" : "rgba(228, 228, 228, 0.7)",
+        "DynamicBubblesBorder" :"rgba(207, 207, 207, 0.7)",
+        "DynamicBubblesShadow" :"0 0 40px 5px rgba(198, 198, 198, 1)",
+
+        "BlackChunk":"#202020",
+        "WhiteChunk":"#cfcfcf",
+
+        "DBTitle": "#474747",
+        "DBContent" : "#5c5c5c",
+        "DBType" : "#393939",
+        "DBBottomBar" : "#484848",
+
+        "DBTextShadow" : "rgba(255, 255, 255, 0.75) 0 0 20px",
+
+        "Background" : "#ececec",
+
+        "DMControlBarBackground":"rgba(244, 244, 244, 0.5)",
+        "DMControlBarShadow":"0 0 10px 10px rgba(255, 255, 255, 0.2)",
+        "DMControlBarBorder":"rgba(207, 207, 207, 0.75)",
+
+        "DMControlBarMidBackground" : "rgb(186, 186, 186)",
+        "DMControlBarMidShadow" : "0 0 10px 10px rgba(227, 227, 227, 0.73)",
+
+        "DMControlBarRopeBackground" : "rgb(186, 186, 186)",
+        "DMControlBarGripBorder" : "rgb(186, 186, 186)",
+    }
+}
+
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function PaintUp(){
+    let Album = isDarkMode ? Colors.Dark : Colors.Light;
+
+    for (let i in Album) {
+        let Elements = [];
+        let ColorPath = "";
+
+        switch (i) {
+            case "DynamicBubblesBackground":{
+                Elements = document.getElementsByClassName("DynamicBubble");
+                ColorPath = "backgroundColor";
+                break;
+            }
+
+            case "DynamicBubblesBorder":{
+                Elements = document.getElementsByClassName("DynamicBubble");
+                ColorPath = "borderColor";
+                break;
+            }
+
+            case "DynamicBubblesShadow":{
+                Elements = document.getElementsByClassName("DynamicBubble");
+                ColorPath = "boxShadow";
+                break;
+            }
+
+            case "DBTitle":{
+                Elements = document.getElementsByClassName("DynamicBubbleFrameTitle");
+                ColorPath = "color";
+                break;
+            }
+
+            case "DBContent":{
+                Elements = document.getElementsByClassName("DynamicBubbleFrameContent");
+                ColorPath = "color";
+                break;
+            }
+
+            case "DBType":{
+                Elements = document.getElementsByClassName("DynamicBubbleTypeTitle");
+                ColorPath = "color";
+                break;
+            }
+
+            case "DBBottomBar":{
+                Elements = document.getElementsByClassName("DynamicBubbleBottomBar");
+                ColorPath = "color";
+                break;
+            }
+
+            case "DBTextShadow":{
+                Elements = document.getElementsByClassName("DynamicBubble");
+                ColorPath = "textShadow";
+                break;
+            }
+
+            case "Background":{
+                Elements = [document.body];
+                ColorPath = "backgroundColor";
+                break;
+            }
+
+            case "DMControlBarBackground":{
+                Elements = document.getElementsByClassName("DynamicBubbleControlBar");
+                ColorPath = "backgroundColor";
+                break;
+            }
+
+            case "DMControlBarShadow":{
+                Elements = document.getElementsByClassName("DynamicBubbleControlBar")
+                ColorPath = "boxShadow";
+                break;
+            }
+
+            case "DMControlBarBorder":{
+                Elements = document.getElementsByClassName("DynamicBubbleControlBar")
+                console.log(Elements);
+                ColorPath = "borderColor";
+                break;
+            }
+
+            case "DMControlBarMidBackground":{
+                Elements = document.getElementById("DynamicBubbleBase").getElementsByTagName("hr");
+                ColorPath = "backgroundColor";
+                break;
+            }
+
+            case "DMControlBarMidShadow":{
+                Elements = document.getElementById("DynamicBubbleBase").getElementsByTagName("hr");
+                ColorPath = "boxShadow";
+                break;
+            }
+
+            case "DMControlBarGripBorder":{
+                Elements = document.getElementsByClassName("DynamicBubbleControlBarHandleGrip");
+                ColorPath = "borderColor";
+                break;
+            }
+
+            case "DMControlBarRopeBackground":{
+                Elements = document.getElementsByClassName("DynamicBubbleControlBarHandleRope");
+                ColorPath = "backgroundColor";
+                break;
+            }
+
+            case "BlackChunk":{
+                Elements = document.getElementsByClassName("whiteChunk");
+                ColorPath = "backgroundColor";
+                break;
+            }
+
+            case "WhiteChunk":{
+                Elements = document.getElementsByClassName("blackChunk");
+                ColorPath = "backgroundColor";
+                break;
+            }
+        }
+
+        for (let j = 0;j<Elements.length;j++)
+            Elements[j].style[ColorPath] = Album[i];
+
+    }
 }
 
 function checkIfMobile() {
@@ -10,6 +206,7 @@ function checkIfMobile() {
     (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
     return check;
 };
+
 
 let Cube = [];
 
@@ -157,10 +354,13 @@ document.addEventListener("DOMContentLoaded",  async function () {
     let buttonElements = [];
 
     function SummonCube(CubeData,SavePath){
+
         DeleteButtons();
 
         if(SavePath) CubePath[CubePath.length] = structuredClone(CubeData);
         Cube = structuredClone(CubeData);
+
+        PaintUp();
 
         CreateButtons(structuredClone(CubeData));
     }
@@ -184,6 +384,7 @@ document.addEventListener("DOMContentLoaded",  async function () {
     }
 
     function CreateButtons(Cube){
+
         for (let i = 0; i < Cube.Buttons.length; i++) {
             let newOne = document.createElement("div");
             newOne.classList.add("button");
@@ -690,6 +891,7 @@ document.addEventListener("DOMContentLoaded",  async function () {
     }
 
     async function CreateDynamicBubbles(BubbleType,Content){
+
         resetPullUpInfo();
 
         let Base = document.getElementById("DynamicBubbleBase");
@@ -809,6 +1011,8 @@ document.addEventListener("DOMContentLoaded",  async function () {
         newBubble.appendChild(newBubbleBottomBar);
 
         Base.appendChild(newBubble);
+
+        PaintUp();
 
         await TidyUpDynamicBubbles();
     };
@@ -1105,9 +1309,9 @@ document.addEventListener("DOMContentLoaded",  async function () {
             moving(CubeInfo.LastXMoved,CubeInfo.LastYMoved);
         } else if(FinalInfo.Bubble && !Pulling && PullUpInfo.MainPullUpIndex === false){
 
-            FinalInfo.Bubble.style.backgroundColor = "rgba(236,236,236,0.9)";
-            FinalInfo.Bubble.style.borderColor = "rgba(218,218,218,0.9)";
-            FinalInfo.Bubble.style.boxShadow =  "0 0 40px 5px rgba(184, 184, 184, 1)";
+            FinalInfo.Bubble.style.backgroundColor = (isDarkMode ? Colors.Dark : Colors.Light).CLICKON_DynamicBubblesBackground;
+            FinalInfo.Bubble.style.borderColor = (isDarkMode ? Colors.Dark : Colors.Light).CLICKON_DynamicBubblesBorder;
+            FinalInfo.Bubble.style.boxShadow =  (isDarkMode ? Colors.Dark : Colors.Light).CLICKON_DynamicBubblesShadow;
 
             FinalInfo.Bubble.style.transform = "translateX(-50%) translateY(10px)";
 
@@ -1483,9 +1687,9 @@ document.addEventListener("DOMContentLoaded",  async function () {
         for (let i in mouseOnBubbles){
             if(!mouseOnBubbles[i]) continue;
 
-            mouseOnBubbles[i].style.backgroundColor = "rgba(228, 228, 228, 0.7)";
-            mouseOnBubbles[i].style.borderColor = "rgba(207, 207, 207, 0.7)";
-            mouseOnBubbles[i].style.boxShadow =  "0 0 40px 5px rgba(198, 198, 198, 1)";
+            mouseOnBubbles[i].style.backgroundColor = (isDarkMode ? Colors.Dark : Colors.Light).DynamicBubblesBackground;
+            mouseOnBubbles[i].style.borderColor = (isDarkMode ? Colors.Dark : Colors.Light).DynamicBubblesBorder;
+            mouseOnBubbles[i].style.boxShadow =  (isDarkMode ? Colors.Dark : Colors.Light).DynamicBubblesShadow;
 
             mouseOnBubbles[i].style.transform = "translateX(-50%) translateY(0)";
 
@@ -1603,20 +1807,20 @@ document.addEventListener("DOMContentLoaded",  async function () {
                 for (let index = 0; index < StartAtElement.getElementsByTagName("div").length; index++) {
                     let thisElement = StartAtElement.getElementsByTagName("div")[index];
                     if(finalButton === StartAtElement.getElementsByTagName("div")[index]){
-                        thisElement.style.color = "#ffffff";
+                        thisElement.style.color = isDarkMode ? "#272727" : "#ffffff";
 
                         if(ControlButtons[index]["IMPORTANT?"])
-                            thisElement.style.background = "#bf3232e0"
+                            thisElement.style.backgroundColor = "#bf3232e0"
                         else
-                            thisElement.style.background = "#393939e0";
+                            thisElement.style.backgroundColor = isDarkMode ? "rgba(230,230,230,0.88)" :"#393939e0";
 
                     }else{
                         if(ControlButtons[index]["IMPORTANT?"])
                             thisElement.style.color = "#bf3232"
                         else
-                            thisElement.style.color = "#393939";
+                            thisElement.style.color = isDarkMode ? "rgb(230,230,230)" :"#393939";
 
-                        thisElement.style.background = "";
+                        thisElement.style.backgroundColor = "";
                     }
                 }
             }else{
@@ -1756,11 +1960,14 @@ document.addEventListener("DOMContentLoaded",  async function () {
     //
     //
     //### START AT HERE ###
+    PaintUp();
+
     TweenUp(true,.14);
     moving(0,0);
 
     let defaultCube = await loadData("./cube/Default.JSON");
     SummonCube(defaultCube,true);
+
 
     for(let i = 0;i<Cube.Buttons.length;i++)
         Cube.Buttons[i].depth = 0;
